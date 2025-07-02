8d1c6cbe50983f16b8d4490cfa5439d5d4fe314e
Rayyankhan025 <khan182007@gmail.com>
2025-07-02 06:41:38 +0000

    rayyan

---
+++ CFI1/05-arrays.js/08-looping.js
@@ -0,0 +1,28 @@
+// const arr = [10,20,30,40,50,60,70,80,90, {a:1,b:2}, 'rayyan','hello'];
+
+// for(i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// const arr =[[22,33],
+//             [2,3],
+//              [222,333,44,55]]
+
+//              for(let i = 0; i < arr.length; i++){
+//                 for(let j = 0; j < arr[i].length; j++){
+//                     console.log(arr[i][j])
+//                 }
+//              }
+
+// const arr =[[[22,33], [2,3], [222,333,44,55]], [[22,33], [2,3]]]
+
+// for(let i=0; i<arr.length; i++){
+//     for(let j=0; j<arr[i].length; j++){
+//         for(let k=0; k<arr[i][j].length; k++){
+//             console.log(arr[i][j][k])
+//         }
+//     }
+// }
+
+
