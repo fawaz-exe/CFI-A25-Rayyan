b9bcf87eb3e8d792e04bb509f38830a83ae25969
Rayyankhan025 <khan182007@gmail.com>
2025-07-09 06:50:55 +0000

    tpdays work 07-09-25

---
+++ CFI1/08-matrix.js/03-print-matrix.js
@@ -0,0 +1,23 @@
+// 1 2
+// 3 4
+
+/**
+ *  1   2   3
+ *  4    5   6
+ *  7    8   9
+ */
+
+import matrixInput from "./02-input-matrix.js";
+
+function printMatrix(m, nameOfMatrix){
+    // m = matrixInput();
+    console.log(`${nameOfMatrix} :`);
+    for(let i = 0; i<m.length;i++){
+        console.log(m[i].join(' '));
+    }
+    return m;
+}
+
+export default printMatrix;
+
+// printMatrix();
